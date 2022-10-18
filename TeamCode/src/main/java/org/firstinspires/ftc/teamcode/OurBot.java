1fb2df2c527cad8d75e36368f6b5d5bb275f2d7d
TylerSGuy <tylerschwarz@chatham-nj.org>
2022-10-18 12:57:20 -0400

    asfd

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurBot.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class OurBot
+{
+
+
+
+
+
+    public DcMotor leftFront = null;
+    public DcMotor leftBack = null;
+    public DcMotor rightFront = null;
+    public DcMotor rightBack = null;
+
+    public void init(HardwareMap hwMap)
+    {
+        leftFront = hwMap.get(DcMotor.class, "leftFront");
+        leftBack = hwMap.get(DcMotor.class, "leftBack");
+        rightFront = hwMap.get(DcMotor.class, "rightFront");
+        rightBack = hwMap.get(DcMotor.class, "rightBack");
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+
+        leftFront.setMode(RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFront.setMode(RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(RunMode.RUN_USING_ENCODER);
+    }
+}
