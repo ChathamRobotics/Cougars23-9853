b81635d06ee846910eb0bf3f5e227bee7f0685a2
bpalazzi512 <bobbypalazzi@gmail.com>
2022-10-20 14:54:28 -0400

    Sets up driver controller code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainDrive.java
@@ -1,5 +1,65 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-public class MainDrive {
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.OurBot;
+
+
+/*
+    TODO
+    Finish opmode loop
+    figure out how controller is connected
+    set up the speed to be low on one of the joysticks, high on the other
+
+
+
+ */
+
+@TeleOp(name="Basic Drive")
+public class MainDrive extends LinearOpMode {
+    private final OurBot robot = new OurBot();
+    private double basePower = 0.8;
+
+
+    private final double FAST_DRIVE_POWER = 0.8;
+    private final double SLOW_DRIVE_POWER = 0.5;
+    private final double ARM_POWER = 0.4;
+    private final double INTAKE_POWER = 0.8;
+
+
+
+    /*
+    The main loop for the controller
+
+     */
+    public void runOpMode()
+    {
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("Seb", "Is Cool");
+        telemetry.update();
+
+        // Let OurRobot do the heavy lifting of getting and initializing the hardware
+        robot.init(hardwareMap);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+
+
+        // Run until the end of the match (driver presses STOP)
+        while(opModeIsActive())
+        {
+
+
+        }
+
+    }
 }
